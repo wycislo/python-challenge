ae7ec6f6a2679289ef47d57a1fc40cc4f79299f7
wycislo <wycislo@yahoo.com>
2021-08-05 06:32:23 -0500

    aug 5

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,22 @@
 import os
 from pathlib import Path
-
+import datetime
 import csv
 
 # some reason my python environment path will not catch my python-challenge directory correctly
 csvpath = Path('/Users/henrywycislo/DataClass/python-challenge/PyBank/Resources/budget_data.csv')
 
-#csvpath = Path('../Resources/budget_data.csv')
+#csvpath = os.path.join('..','Resources','budget_data.csv')
 
 # read the file 
-with open(csvpath, "r") as csvfile:
-    # specify the delimiter and variable to hold data from file 
-    csvreader = csv.reader(csvfile, delimiter =',')
-    print(csvreader)
-
-    # read the header 
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    print(f"CSV Header:  {csv_header}")
 
-    #read each row 
     for row in csvreader:
-        print(row)
-        thedate = row(0)
-        thePL = row(1)
-print(thedate)
-print(thePL)
+        PLDate = row(0)
+        PLAmount = float(row(1))
+        print(PLDate)
+        print(PLAmount)
+        
