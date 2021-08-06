bc3ca236cdc0a6262924ff37c99939f72c58d4f3
wycislo <wycislo@yahoo.com>
2021-08-06 16:12:20 -0500

    election

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,47 @@
 # Read CSV file, process data, write to output file and to terminal
 import os
 from pathlib import Path
-import datetime
 import csv
 
-csvpath = Path('Resources/election_data.csv')
+
+csvpath = Path('Resources\election_data.csv')
+
+# lists to store data
+
+voter_id = []
+County = []
+CandidateName = []
+Total_Votes = 0
+
+with open(csvpath,"r") as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=',')
+    #skip the header
+    next(csvreader)
+
+    for row in csvreader:
+        voter_id.append(row[0])
+        County.append (row[1])
+        CandidateName.append (row[2])
+        Total_Votes = Total_Votes +1
+
+
+# create dictionay that counts votes for each candidate
+# PLDictionary = dict(zip(CandidateName,voter_id))
+VoteDictionary = {}
+for Canidate in CandidateName:
+    if Canidate in VoteDictionary:
+        VoteDictionary[Canidate] +=1
+    else:
+        VoteDictionary[Canidate] = 1
+
+
+print(Total_Votes)
+
+print(VoteDictionary)
+
+for key in VoteDictionary:
+    value = VoteDictionary[key]
+    print("Candidate: " + key, value)
+
+
+
