e7570116c5ded9a8481f84ddcd9fcd303a79bca5
wycislo <wycislo@yahoo.com>
2021-08-06 10:05:41 -0500

    PyPoll Start

---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+# PyBank
+# Read CSV file, process data, write to output file and to terminal
+import os
+from pathlib import Path
+import datetime
+import csv
+
+csvpath = Path('Resources/election_data.csv')
